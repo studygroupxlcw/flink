3b8010e6bdc41f7443d5387d0853d6455cdac5bc
dreamffss <xsyyff@live.com>
2020-02-11 14:05:59 +0800

    细化说明

---
+++ src/main/java/study/flink/bootstrap/QuickStart.java
@@ -0,0 +1,12 @@
+package study.flink.bootstrap;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class QuickStart {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    }
+
+}
